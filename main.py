e081d63de1fa81f9c28c54135307d7a9537cb866
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-06-12 17:06:50 -0600

    Pop random elements from file list

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import random
+
+hex_source_file = "needed"
+hex_dest_files = ["gou", "iron", "elizibet"]
+
+if __name__ == "__main__":
+
+    with open(hex_source_file, mode='r+') as hex_source:
+        needed_hexes = hex_source.readlines()
+        new_hex = random.choice(needed_hexes)
+        needed_hexes.remove(new_hex)
+        print(new_hex)
+        hex_source.truncate(0)
+        hex_source.writelines(needed_hexes)
+
