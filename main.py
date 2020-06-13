b8c02d06ac287b5d0268b19e62cbf6f2b62fa452
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-06-12 20:31:04 -0600

    Check for empty source hex file

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ if __name__ == "__main__":
 
     with open(hex_source_file, mode='r+') as hex_source:
         needed_hexes = hex_source.readlines()
+        if len(needed_hexes) == 0:
+            print("All hexes accounted for!")
+            sys.exit(0)
+
         new_hex = random.choice(needed_hexes)
         needed_hexes.remove(new_hex)
         print(new_hex)
