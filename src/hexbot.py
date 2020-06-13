81ea2e463874d2b8ea4e1775a9897a01ba6ad776
Dylan Craine <dylan.craine@deepwatercreations.com>
2020-06-13 16:51:59 -0600

    Bot can read messages and find subcommands

--- src/hexbot.py
+++ src/hexbot.py
@@ -13,4 +13,26 @@ client = discord.Client()
 async def on_ready():
     print(f"{client.user} has connected to Discord")
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!hex"):
+        args = message.content.split()
+        if len(args) == 1:
+            subcommand = "help"
+        else:
+            subcommand = args[1]
+
+        if subcommand in ["help", "--help", "-h", "h", "?"]:
+            print("Help requested")
+        elif subcommand in ["get", "--get", "-g", "g"]:
+            print("User wants a hex")
+        elif subcommand in ["submit", "--submit", "-s", "s"]:
+            print("User wants to submit a hex")
+        else:
+            print("I dunno what {} is.".format(subcommand))
+
+
 client.run(TOKEN)
